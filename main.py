420869ddfe1df4f26d9d165e7f4af8c3729f928a
Bernardo <eubernardoassis@gmail.com>
2023-09-18 22:58:58 -0300

    Baixando audio ou video via link (CLI)

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from pytube import YouTube
 
 get_video = input("Insira o link do video: ")
 yt = YouTube(get_video)
+print(f"Titulo: {yt.title}")
 
-print(yt.title)
 
+while True:
+    op = int(input("Você quer audio[1] ou video[2]? "))
+    if op == 1:
+        #Baixar audio
+        print('-- Baixando o audio --')
+        audio = yt.streams.filter(only_audio=True).first()
+        audio.download()
+        break
 
-#Baixar mp4
-stream = yt.streams.filter(file_extension='mp4').first()
-stream.download()
+    elif op == 2:
+        #Baixar video
+        print('-- Baixando o video --')
+        video = yt.streams.filter(file_extension='mp4').first()
+        video.download()
+        break
+
+    else:
+        print('Insira um número valido')
