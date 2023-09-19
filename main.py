6c4c476f3659aa24a6cddbdd79a4212b996a93be
Bernardo <eubernardoassis@gmail.com>
2023-09-18 22:44:49 -0300

    baixar mp4

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ get_video = input("Insira o link do video: ")
 yt = YouTube(get_video)
 
 print(yt.title)
+
+
+#Baixar mp4
+stream = yt.streams.filter(file_extension='mp4').first()
+stream.download()
