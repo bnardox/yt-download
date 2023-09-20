3e2239f5f0beb12052d634b745be3750828fc724
Bernardo <eubernardoassis@gmail.com>
2023-09-19 23:20:08 -0300

    configurando botoes e personalizando

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from pytube import YouTube
 from customtkinter import *
+from time import sleep
 
 #Configurando janela
 app = CTk()
 app .title("Download PY")
 app.geometry("500x340")
-
 set_appearance_mode("Dark")
 set_default_color_theme("green")
+
 
 #função para baixar o video
 def download_video():
     link = text_box.get()
     yt = YouTube(link)
+    wait_label = CTkLabel(master=app, text=f'Título: {yt.title}', width=10, height=25)
+    wait_label.place(relx=0.35, rely=0.65)
+    wait_label.cget("font").configure(size=18)
     video = yt.streams.get_highest_resolution()
     video.download()
 
@@ -20,6 +24,9 @@ def download_video():
 def download_audio():
     link = text_box.get()
     yt = YouTube(link)
+    wait_label = CTkLabel(master=app, text=f'Título: {yt.title}', width=10, height=25)
+    wait_label.place(relx=0.35, rely=0.65)
+    wait_label.cget("font").configure(size=18)
     audio = yt.streams.filter(only_audio=True).first()
     audio.download()
 
@@ -37,8 +44,8 @@ link = text_box.get()
 
 #label de espera
 wait_label = CTkLabel(master=app, text="Aguardando link...", width=10, height=25)
-wait_label.place(relx=0.38, rely=0.65)
-
+wait_label.place(relx=0.36, rely=0.65)
+wait_label.cget("font").configure(size=18)
 
 #Botão de download do video
 video_btn = CTkButton(master=app, text="Download video", width=120, height=25, corner_radius=8, command=download_video)
