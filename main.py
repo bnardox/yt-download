9a94be43e4e006168f0566a89c793032c30a898d
Bernardo <eubernardoassis@gmail.com>
2023-09-19 22:21:21 -0300

    input do link

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ set_appearance_mode("Dark")
 set_default_color_theme("green")
 
 
+label = CTkLabel(master=app, text="Insira o link aqui:", width=10, height=25)
+label.place(relx=0.20, rely=0.12)
+
+text_box = CTkEntry(master=app, width=280, height=25, corner_radius=8)
+text_box.place(relx=0.20, rely=0.20)
+link = text_box.get()
+
 app.mainloop()
 
-get_video = input("Insira o link do video: ")
+'''get_video = input("Insira o link do video: ")
 yt = YouTube(get_video)
 print(f"Titulo: {yt.title}")
 
@@ -34,5 +41,5 @@ while True:
         break
 
     else:
-        print('Insira um número valido')
+        print('Insira um número valido')'''
 
