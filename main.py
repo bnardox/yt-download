f7708b213e86ebe9580a22cb94a645d1543df759
Bernardo <eubernardoassis@gmail.com>
2023-09-19 22:42:01 -0300

    Botoes de dowload e suas funcoes

--- main.py
+++ main.py
@@ -9,37 +9,37 @@ app.geometry("500x340")
 set_appearance_mode("Dark")
 set_default_color_theme("green")
 
+#função para baixar o video
+def download_video():
+    link = text_box.get()
+    yt = YouTube(link)
+    video = yt.streams.filter(file_extension='mp4').first()
+    video.download()
 
+#função para baixar o audio
+def download_audio():
+    link = text_box.get()
+    yt = YouTube(link)
+    audio = yt.streams.filter(only_audio=True).first()
+    audio.download()
+
+
+
+#Label do input
 label = CTkLabel(master=app, text="Insira o link aqui:", width=10, height=25)
 label.place(relx=0.20, rely=0.12)
 
+#Input do link
 text_box = CTkEntry(master=app, width=280, height=25, corner_radius=8)
 text_box.place(relx=0.20, rely=0.20)
 link = text_box.get()
 
+#Botão de download do video
+video_btn = CTkButton(master=app, text="Download video", width=120, height=25, corner_radius=8, command=download_video)
+video_btn.place(relx=0.22, rely=0.35)
+
+#Botão de download do audio
+video_btn = CTkButton(master=app, text="Download audio", width=120, height=25, corner_radius=8, command=download_audio)
+video_btn.place(relx=0.50, rely=0.35)
+
 app.mainloop()
-
-'''get_video = input("Insira o link do video: ")
-yt = YouTube(get_video)
-print(f"Titulo: {yt.title}")
-
-
-while True:
-    op = int(input("Você quer audio[1] ou video[2]? "))
-    if op == 1:
-        #Baixar audio
-        print('-- Baixando o audio --')
-        audio = yt.streams.filter(only_audio=True).first()
-        audio.download()
-        break
-
-    elif op == 2:
-        #Baixar video
-        print('-- Baixando o video --')
-        video = yt.streams.filter(file_extension='mp4').first()
-        video.download()
-        break
-
-    else:
-        print('Insira um número valido')'''
-
